bfcfd533b249611e8d2ac633c64ba88c289402dd
“Tatu <koivisto.tatu@gmail.com>
2017-02-04 23:59:00 +0200

    new effects to the icons

--- js/custom.js
+++ js/custom.js
@@ -8,12 +8,14 @@ $("#logo-style").click(hideCover);
 
 
 function hideCover() {
-	document.getElementById("full-image-style").style.display = 'block';
-	//document.getElementById("main-navigation").style.display = 'block';
 
 
+	$("#full-image-style").show();
 	$("#cover-div-right").hide(1000);
     $("#cover-div-left").hide(1000);
-    $("#logo-container").delay(1200).animate({top: "20%"},2000);
+    $("#logo-container").delay(500).animate({top: "20%"},1500);
+    $(".content-div-1").delay(1500).show(500);
+    $(".content-div-2").delay(1750).show(500);
+    $(".content-div-3").delay(2000).show(500);
 }
 
