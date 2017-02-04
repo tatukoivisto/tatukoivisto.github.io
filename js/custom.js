1cd106bdc9b9222f28529498e712879f38ea92df
“Tatu <koivisto.tatu@gmail.com>
2017-02-04 22:07:29 +0200

    tausta ja logon liike

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
+
 $("#logo-style").click(hideCover);
+
+
+
 });
 
 
 function hideCover() {
-    document.getElementById("cover-div-right").style.display = 'none';
-    document.getElementById("cover-div-left").style.display = 'none';
 	document.getElementById("full-image-style").style.display = 'block';
-	document.getElementById("main-navigation").style.display = 'block';
+	//document.getElementById("main-navigation").style.display = 'block';
+
+
+	$("#cover-div-right").hide(1000);
+    $("#cover-div-left").hide(1000);
+    $("#logo-container").delay(1200).animate({top: "20%"},2000);
 }
 
-
-/*$(document).ready(hideCover)
-document.getElementById("logo-style").addEventListener("click", hideCover);*/
