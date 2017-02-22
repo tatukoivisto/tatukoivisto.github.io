b3c26c1f456635fb28f8c7b0f2af0d320c19e999
“Tatu <koivisto.tatu@gmail.com>
2017-02-22 23:51:14 +0200

    dynamic

---
+++ js/dynamicstory.js
@@ -0,0 +1,12 @@
+$.ajax({
+  dataType: "json",
+  url: https://koivist9.firebaseio.com/.json,
+  data: data,
+  success: success
+});
+
+
+/*var database = firebase.database();
+var databaseRef = firebase.database().ref('content/');
+var testi = firebase.database().ref("https://koivist9.firebaseio.com/");
+*/
